7fac362c5cc5d1dca87a89201885c5735c1724d9
TuTuanLe <tutuanletkpt@gmail.com>
2024-06-15 23:57:55 +0700

    feat: update ui for experience

--- react-profile-website/src/components/Services/Services.js
+++ react-profile-website/src/components/Services/Services.js
@@ -7,79 +7,96 @@ const Services = () => {
       <h5>What I Dffer</h5>
       <h2>Services</h2>
       <div className="container services__container">
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
+        <div className="relative group">
+          <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+          <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+            <article className="service">
+              <div className="service__head">
+                <h3>UI/UX Design</h3>
+              </div>
+
+              <ul className="service__list">
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Figma</p>
+                </li>
+
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Photoshop.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Adobe XD.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Marvel.</p>
+                </li>
+              </ul>
+            </article>
           </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Figma</p>
-            </li>
+        </div>
 
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Photoshop.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Adobe XD.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Marvel.</p>
-            </li>
-          </ul>
-        </article>
+        <div className="relative group">
+          <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+          <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+            <article className="service">
+              <div className="service__head">
+                <h3>Web Devoloper</h3>
+              </div>
+              <ul className="service__list">
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Html, CSS, Javascript</p>
+                </li>
 
-        <article className="service">
-          <div className="service__head">
-            <h3>Web Devoloper</h3>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>TypeScript.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>React JS, Redux.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>.....</p>
+                </li>
+              </ul>
+            </article>
           </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Html, CSS, Javascript</p>
-            </li>
+        </div>
 
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>TypeScript.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>React JS, Redux.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>.....</p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service__head">
-            <h3>Content Creation</h3>
+        <div className="relative group">
+          <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+          <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+            <article className="service">
+              <div className="service__head">
+                <h3>Content Creation</h3>
+              </div>
+              <ul className="service__list">
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Get Api Link Youtube Chat App</p>
+                </li>
+
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Share Positive Statistics.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Create content youtube.</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" />
+                  <p>Write attractive and neat captions.</p>
+                </li>
+              </ul>
+            </article>
           </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Get Api Link Youtube Chat App</p>
-            </li>
-
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Share Positive Statistics.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Create content youtube.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Write attractive and neat captions.</p>
-            </li>
-          </ul>
-        </article>
+        </div>
       </div>
     </section>
   );
--- react-profile-website/src/components/experience/Experience.js
+++ react-profile-website/src/components/experience/Experience.js
@@ -1,99 +1,110 @@
 import React from 'react';
-import './experience.css';
 import { BsPatchCheckFill } from 'react-icons/bs';
+import './experience.css';
 const Experience = () => {
   return (
     <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
       <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Frontend Developer</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>HTML</h4>
-                <small className="text-light">Experience</small>
+        <div className="relative group">
+          <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+          <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+            <div className="experience__frontend">
+              <h3>Frontend Developer</h3>
+              <div className="experience__content">
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>HTML</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>CSS</h4>
+                    <small className="text-light">Intermediate</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>BootStrap</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>Tailwind</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>React</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
               </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>BootStrap</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Taiwind</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
+            </div>
           </div>
         </div>
-        <div className="experience__backend">
-          <h3>Backend Developer</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <div>
-                  <h4>.NET</h4>
-                  <small className="text-light">Experience</small>
-                </div>
+
+        <div className="relative group">
+          <div className="w-full h-full absolute -inset-1 bg-gradient-to-r from-teal-100 to-teal-900 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
+          <div className="relative w-full p-4 bg-white bg-opacity-10 backdrop-blur-lg rounded-lg">
+            <div className="experience__backend">
+              <h3>Backend Developer</h3>
+              <div className="experience__content">
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <div>
+                      <h4>.NET</h4>
+                      <small className="text-light">Experience</small>
+                    </div>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>JAVA</h4>
+                    <small className="text-light">Intermediate</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>PHP</h4>
+                    <small className="text-light">Intermediate</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>MONGODB</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>SQL SERVER</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
+                <article className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>PYTHON</h4>
+                    <small className="text-light">Experience</small>
+                  </div>
+                </article>
               </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JAVA</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PHP</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MONGODB</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>SQL SERVER</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PYTHON</h4>
-                <small className="text-light">Experience</small>
-              </div>
-            </article>
+            </div>
           </div>
         </div>
       </div>
